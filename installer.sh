733b011ed1afadc43e320acdc3de945cbeea030d
itsharsh <hkumarsingh142@gmail.com>
2019-09-03 16:49:17 +0530

    Requirements updated

--- installer.sh
+++ installer.sh
@@ -3,20 +3,20 @@ sudo mkdir datasets
 sudo mkdir tempaudio
 sudo mkdir data
 sudo apt autoremove -y
-sudo apt-get update -y
 sudo apt update -y
+sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
+sudo apt-get install flac -y
 sudo apt-get install python3 -y
-sudo apt-get -y install python3-dev python3-pip
-sudo apt-get install python3-matplotlib -y
+sudo apt-get install python3-opencv -y
 sudo apt-get install python3-pyaudio -y
-sudo apt-get install libhdf5-dev libhdf5-serial-dev -y
-sudo apt-get install libqtwebkit4 libqt4-test -y
+sudo apt-get install python3-matplotlib -y
+sudo apt-get install python3-dev python3-pip -y
 sudo apt install libjasper1 -y
 sudo apt-get install libatlas-base-dev -y
-sudo apt-get install python3-opencv -y
+sudo apt-get install libqtwebkit4 libqt4-test -y
+sudo apt-get install libhdf5-dev libhdf5-serial-dev -y
 sudo apt-get install libatlas-base-dev -y
-sudo apt install virtualenv python3-virtualenv -y
 
 sudo pip3 install -r requirements.txt
